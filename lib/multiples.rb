e0dbee2f4a935346a115245de545315930a91d76
kavunshiva <kavunshiva@gmail.com>
2017-04-17 11:06:48 -0400

    initial commit

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
-# Enter your procedural solution here!
+# naive solution - room for algorithmic improvement
+def collect_multiples(limit)
+  (1...limit).select do |num|
+    num % 3 == 0 or num % 5 == 0
+  end
+end
+
+def sum_multiples(limit)
+  sum = 0
+  collect_multiples(limit).each do |multiple|
+    sum += multiple
+  end
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
-# Enter your object-oriented solution here!
+# naive solution - room for algorithmic improvement
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    (1...@limit).select do |num|
+      num % 3 == 0 or num % 5 == 0
+    end
+  end
+
+  def sum_multiples
+    sum = 0
+    collect_multiples.each do |multiple|
+      sum += multiple
+    end
+    sum
+  end
+end
